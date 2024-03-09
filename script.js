8556bd33b368cbc55388569ef5d6d2aadb0f4d7a
Anurag Verma <anuragvermacoding@gmail.com>
2024-03-09 12:00:30 +0530

    home page

---
+++ script.js
@@ -0,0 +1,9 @@
+function requestSwap() {
+    // Add functionality for requesting a book swap
+    alert('Swap request sent successfully!');
+}
+
+function redirectTo(page) {
+    // Add functionality to redirect to sign-up or login page
+    window.location.href = page;
+}
