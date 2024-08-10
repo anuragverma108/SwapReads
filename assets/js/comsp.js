414242a3d6d8b89dcc31fa4437367fb8d2502f3e
Anjaliavv51 <154777864+Anjaliavv51@users.noreply.github.com>
2024-08-10 12:05:32 +0530

    Added reveal on scroll to comsp page
    #2810

---
+++ assets/js/comsp.js
@@ -0,0 +1,8 @@
+ScrollReveal({
+    //reset: true ,
+    distance: '1000px',
+    duration: 2500,
+    delay: 400
+  });
+  ScrollReveal().reveal('.section__title ,.section__subtitle', { delay: 400, origin: 'left' });
+  ScrollReveal().reveal('.spotlight__grid .spotlight__card', { delay: 700, origin: 'bottom' ,interval:300});
